9ddeeaea6ec718ebc0fe3056948695d7578411c7
Hanumath <hvp6533@mavs.uta.edu>
2025-07-18 16:21:15 -0500

    Day 6 match still has project not found error

--- handlers/match.go
+++ handlers/match.go
@@ -1,63 +1,63 @@
 package handlers
 
 import (
-    "encoding/json"
-    "net/http"
-    "strconv"
-    "strings"
+	"encoding/json"
+	"net/http"
+	"strconv"
+	"strings"
 
-    "github.com/Hanumath1006/skillsync/models"
-    "github.com/Hanumath1006/skillsync/middleware"
-    "github.com/gorilla/mux"
+	"github.com/Hanumath1006/skillsync/middleware"
+	"github.com/Hanumath1006/skillsync/models"
+	"github.com/gorilla/mux"
 )
 
 func MatchUsers(w http.ResponseWriter, r *http.Request) {
-    user := middleware.GetUserFromContext(r)
-    if user == nil {
-        http.Error(w, "Unauthorized", http.StatusUnauthorized)
-        return
-    }
+	user := middleware.GetUserFromContext(r)
+	if user == nil {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
 
-    vars := mux.Vars(r)
-    projectID, err := strconv.Atoi(vars["projectId"])
-    if err != nil {
-        http.Error(w, "Invalid project ID", http.StatusBadRequest)
-        return
-    }
+	vars := mux.Vars(r)
+	projectID, err := strconv.Atoi(vars["projectId"])
+	if err != nil {
+		http.Error(w, "Invalid project ID", http.StatusBadRequest)
+		return
+	}
 
-    var project *models.Project
-    for _, p := range models.Projects {
-        if p.ID == projectID {
-            project = &p
-            break
-        }
-    }
+	var project *models.Project
+	for i := range models.Projects {
+		if models.Projects[i].ID == projectID {
+			project = &models.Projects[i]
+			break
+		}
+	}
 
-    if project == nil {
-        http.Error(w, "Project not found", http.StatusNotFound)
-        return
-    }
+	if project == nil {
+		http.Error(w, "Project not found", http.StatusNotFound)
+		return
+	}
 
-    var matchedUsers []models.User
-    for _, u := range models.Users {
-        if hasMatchingSkills(u.Skills, project.RequiredSkills) {
-            matchedUsers = append(matchedUsers, u)
-        }
-    }
+	var matchedUsers []models.User
+	for _, u := range models.Users {
+		if hasMatchingSkills(u.Skills, project.RequiredSkills) {
+			matchedUsers = append(matchedUsers, u)
+		}
+	}
 
-    json.NewEncoder(w).Encode(matchedUsers)
+	json.NewEncoder(w).Encode(matchedUsers)
 }
 
 func hasMatchingSkills(userSkills, requiredSkills []string) bool {
-    userSkillSet := make(map[string]bool)
-    for _, s := range userSkills {
-        userSkillSet[strings.ToLower(s)] = true
-    }
+	userSkillSet := make(map[string]bool)
+	for _, s := range userSkills {
+		userSkillSet[strings.ToLower(s)] = true
+	}
 
-    for _, rs := range requiredSkills {
-        if userSkillSet[strings.ToLower(rs)] {
-            return true
-        }
-    }
-    return false
+	for _, rs := range requiredSkills {
+		if userSkillSet[strings.ToLower(rs)] {
+			return true
+		}
+	}
+	return false
 }
