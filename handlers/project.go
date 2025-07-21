e0066cd24101ad89a1f719f1b71796af55904f8d
Hanumath <hvp6533@mavs.uta.edu>
2025-07-21 13:59:09 -0500

    models.Projects fixed

--- handlers/project.go
+++ handlers/project.go
@@ -1,51 +1,48 @@
 package handlers
 
 import (
-    "encoding/json"
-    "net/http"
-    "github.com/Hanumath1006/skillsync/models"
-    "github.com/Hanumath1006/skillsync/middleware"
+	"encoding/json"
+	"net/http"
 
+	"github.com/Hanumath1006/skillsync/middleware"
+	"github.com/Hanumath1006/skillsync/models"
 )
 
-var (
-    projects        []models.Project
-    projectIDCounter = 1
-)
+var projectIDCounter = 1
 
 // POST /projects
 func CreateProject(w http.ResponseWriter, r *http.Request) {
-    user := middleware.GetUserFromContext(r)
-    if user == nil {
-        http.Error(w, "Unauthorized", http.StatusUnauthorized)
-        return
-    }
+	user := middleware.GetUserFromContext(r)
+	if user == nil {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
 
-    var input struct {
-        Title         string   `json:"title"`
-        Description   string   `json:"description"`
-        RequiredSkills []string `json:"required_skills"`
-    }
+	var input struct {
+		Title          string   `json:"title"`
+		Description    string   `json:"description"`
+		RequiredSkills []string `json:"required_skills"`
+	}
 
-    if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
-        http.Error(w, "Invalid body", http.StatusBadRequest)
-        return
-    }
+	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+		http.Error(w, "Invalid body", http.StatusBadRequest)
+		return
+	}
 
-    newProject := models.Project{
-        ID:             projectIDCounter,
-        Title:          input.Title,
-        Description:    input.Description,
-        RequiredSkills: input.RequiredSkills,
-        OwnerID:        user.UserID,
-    }
-    projectIDCounter++
-    projects = append(projects, newProject)
+	newProject := models.Project{
+		ID:             projectIDCounter,
+		Title:          input.Title,
+		Description:    input.Description,
+		RequiredSkills: input.RequiredSkills,
+		OwnerID:        user.UserID,
+	}
+	projectIDCounter++
+	models.Projects = append(models.Projects, newProject)
 
-    json.NewEncoder(w).Encode(newProject)
+	json.NewEncoder(w).Encode(newProject)
 }
 
 // GET /projects
 func GetProjects(w http.ResponseWriter, r *http.Request) {
-    json.NewEncoder(w).Encode(projects)
+	json.NewEncoder(w).Encode(models.Projects)
 }
--- models/project.go
+++ models/project.go
@@ -1,11 +1,11 @@
 package models
 
 type Project struct {
-	ID            int      `json:"id"`
-	Title         string   `json:"title"`
-	Description   string   `json:"description"`
-	RequiredSkills []string `json:"required_skills"`
-	OwnerID       int      `json:"owner_id"` // Linked to user
+    ID             int      `json:"id"`
+    Title          string   `json:"title"`
+    Description    string   `json:"description"`
+    RequiredSkills []string `json:"required_skills"`
+    OwnerID        int      `json:"owner_id"`
 }
 
 var Projects []Project
